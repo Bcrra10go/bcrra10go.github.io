2a7e81000fce221c94a3f6f1a0f1d18d904d47b9
Bcrra10go <89609021+Bcrra10go@users.noreply.github.com>
2023-01-20 17:26:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const d = new Date();
 var clicks = 0;
 var keys = 0;
 var k1 = 0, k2 = 0, k3 = 0, k4 = 0, k5 = 0;
+var c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0;
 
 
 function onClick(){
@@ -49,6 +50,40 @@ function time(){
     const b = new Date();
     var sec2 = Math.round((b.getTime() - d.getTime()) / 1000);
     document.getElementById("time").innerHTML = sec2;
+}
+
+function countCharacter1(obj){
+    c1 = obj.value.length;
+    document.getElementById("firstnamec").innerHTML = c1;
+    characters();
+}
+
+function countCharacter2(obj){
+    c2 = obj.value.length;
+    document.getElementById("emailc").innerHTML = c2;
+    characters();
+}
+
+function countCharacter3(obj){
+    c3 = obj.value.length;
+    document.getElementById("usernamec").innerHTML = c3;
+    characters();
+}
+
+function countCharacter4(obj){
+    c4 = obj.value.length;
+    document.getElementById("passwordc").innerHTML = c4;
+    characters();
+}
+
+function countCharacter5(obj){
+    c5 = obj.value.length;
+    document.getElementById("passwordconfirmationc").innerHTML = c5;
+    characters();
+}
+
+function characters(){
+    document.getElementById("characters").innerHTML = c1 + c2 + c3 + c4 + c5;
 }
 
 setInterval(time, 1000);
