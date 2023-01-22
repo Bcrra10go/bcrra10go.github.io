d12862aabf209df14c2ad6000f16051dc010a66a
Bcrra10go <89609021+Bcrra10go@users.noreply.github.com>
2023-01-22 16:22:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const d = new Date();
-
+var count = 0;
 var clicks = 0;
 var keys = 0;
-var k1 = 0, k2 = 0, k3 = 0, k4 = 0, k5 = 0;
-var c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0;
+var ch = 0;
 var map = {};
+
+function onKeyDown(obj){
+    count = obj.value.length;
+}
 
 
 function onClick(){
@@ -21,16 +24,14 @@ function onKeyPress(obj){
     var element = obj.nextSibling.nextSibling;
 
     var child = element.children[1];
-    console.log(child);
     var x = parseInt(child.innerHTML);
     child.innerHTML = x + 1;
 
     element = element.nextSibling.nextSibling;
 
     child = element.children[1];
-    console.log(child);
     child.innerHTML = obj.value.length;
-    map[obj] = obj.value.length;
+    ch = ch + (obj.value.length - count);
     totalKeys();
     characters();
 }
@@ -42,12 +43,14 @@ function time(){
 }
 
 function characters(){
-    var y = 0;
-    for(var x in map){
-        y = y + map[x];
-    }
+    document.getElementById("characters").innerHTML = ch;
+}
 
-    document.getElementById("characters").innerHTML = y;
+function reveal(){
+    var x = document.getElementsByClassName("hidden");
+    for(var i = 0; i < x.length; i++){
+        x[i].style.display = "block";
+    }
 }
 
 setInterval(time, 1000);
