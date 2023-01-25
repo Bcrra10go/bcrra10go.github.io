a85f2685a2cdea68079f4d1b1e15bc8c28bdc33e
Bcrra10go <89609021+Bcrra10go@users.noreply.github.com>
2023-01-25 14:45:19 +0100

    Update script_regex.js

--- script_regex.js
+++ script_regex.js
@@ -1,21 +1,25 @@
 function userID(){
     let obj = document.getElementById("userID");
     if(obj.value.length == 0){
-        console.log("User ID alert");
+        required(document.getElementById("usererror"));
         return;
     }
     var n = obj.value.match(/[$&+,:;=?@#|'<>.^*()%!A-z0-9]/g);
     var x = obj.value.search(/[A-Z]/);
     var y = n[obj.value.length - 1].search(/[$&+,:;=?@#|'<>.^*()%!0-9]/)
     if(n == null || n.length > 12 || n.length < 5 || x != 0 || y != 0){
-        console.log("User ID alert");
+        document.getElementById("usererror").innerHTML = "ID must be of length 5 to 12, must start with a capital letter and end with a number or special character";
+    }
+
+    else{
+        valid(document.getElementById("usererror")); 
     }
 }
 
 function password(){
     let obj = document.getElementById("password");
     if(obj.value.length == 0){
-        console.log("Password alert");
+        required(document.getElementById("pwderror"));
         return;
     }
 
@@ -25,34 +29,46 @@ function password(){
     var y = obj.value.search(/[a-z]/);
     var z = obj.value.search(/[$&+,:;=?@#'<>.^*()%!]/g);
     if(w == -1 || x == -1 || y == -1 || z == -1 || n.length < 12){
-        console.log("Password alert");
+        document.getElementById("pwderror").innerHTML = "Password must be at least 12 characters long, contain one upper case, one lower case, one numerical and one special character";
+    }
+
+    else{
+        valid(document.getElementById("pwderror")); 
     }
 }
 
 function firstName(){
     let obj = document.getElementById("firstname");
     if(obj.value.length == 0){
-        console.log("Name alert");
+        required(document.getElementById("nameerror"));
         return;
     }
     var n = obj.value.match(/[$&+,:;=?@#|'<>.^*()%!0-9]/g);
     if(n != null){
-        console.log("Name alert");
+        document.getElementById("nameerror").innerHTML = "Name can only contain letters";
+    }
+
+    else{
+        valid(document.getElementById("nameerror")); 
     }
 }
 
 function country(){
     let obj = document.getElementById("country");
     if(obj.value.length == 0){
-        console.log("Country alert");
+        required(document.getElementById("countryerror"));
         return;
+    }
+
+    else{
+        valid(document.getElementById("countryerror")); 
     }
 }
 
 function zipCode(){
     let obj = document.getElementById("zip");
     if(obj.value.length != 6){
-        console.log("Zip Code alert");
+        document.getElementById("ziperror").innerHTML = "ZIP Code must be 6 characters long";
         return;
     }
 
@@ -63,7 +79,7 @@ function zipCode(){
     var z = obj.value.search(/[$&+,:;=?@#'<>.^*()%!]/g);
 
     if(w == -1 || x == -1 || y != -1 || z != -1 || n.length != 6){
-        console.log("Zip Code alert");
+        document.getElementById("ziperror").innerHTML = "ZIP Code must can only contain upper case letters or numbers";
     }
 
     var a = n[0].search(/[0-9]/);
@@ -74,14 +90,18 @@ function zipCode(){
     var f = n[5].search(/[A-Z]/);
 
     if(a != 0 || b != 0 || c != 0 || d != 0 || e != 0 || f != 0){
-        console.log("Zip Code alert");
+        document.getElementById("ziperror").innerHTML = "ZIP Code must be 4 numbers first and 2 letters in the end";
+    }
+
+    else{
+        valid(document.getElementById("ziperror")); 
     }
 }
 
 function email(){
     let obj = document.getElementById("email");
     if(obj.value.length == 0){
-        console.log("Email alert");
+        required(document.getElementById("emailerror"));
         return;
     }
 
@@ -90,24 +110,44 @@ function email(){
     var y = obj.value.search("@");
 
     if(x != -1 || y == -1 || y == (obj.value.length - 1)){
-        console.log("Email alert");
+        document.getElementById("emailerror").innerHTML = "Please enter valid email";
+    }
+
+    else{
+        valid(document.getElementById("emailerror")); 
     }
 }
 
 function sex(){
     let obj = document.getElementById("sex");
     if(obj.value.length == 0){
-        console.log("Sex alert");
+        required(document.getElementById("sexerror"));
         return;
+    }
+
+    else{
+        valid(document.getElementById("sexerror")); 
     }
 }
 
 function language(){
     let obj = document.getElementById("language");
     if(obj.value.length == 0){
-        console.log("Language alert");
+        required(document.getElementById("languageerror"));
         return;
     }
+
+    else{
+        valid(document.getElementById("languageerror")); 
+    }
+}
+
+function required(obj){
+    obj.innerHTML = "This field is required";
+}
+
+function valid(obj){
+    obj.innerHTML= "✓";
 }
 
 function onClick(){
